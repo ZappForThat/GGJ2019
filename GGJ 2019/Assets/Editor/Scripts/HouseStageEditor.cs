fcd68a1a29789d1457c11a8b9cca998f822828b9
chaosed0 <chaosed0@gmail.com>
2019-01-26 21:28:06 -0800

    House stage editor

---
+++ GGJ 2019/Assets/Editor/Scripts/HouseStageEditor.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+
+[CustomEditor(typeof(HouseStage))]
+public class HouseStageEditor : Editor
+{
+    SerializedProperty requiredItemProperty;
+    SerializedProperty nailsProperty;
+    SerializedProperty sawLogProperty;
+
+    private void OnEnable()
+    {
+        requiredItemProperty = serializedObject.FindProperty("requiredItem");
+        nailsProperty = serializedObject.FindProperty("nails");
+        sawLogProperty = serializedObject.FindProperty("sawLog");
+    }
+
+    public override void OnInspectorGUI()
+    {
+        HouseStage houseStage = (HouseStage)target;
+        serializedObject.Update();
+
+        EditorGUILayout.PropertyField(requiredItemProperty);
+
+        if (houseStage.requiredItem == Item.Hammer)
+        {
+            EditorGUILayout.PropertyField(nailsProperty, true);
+        }
+        else if (houseStage.requiredItem == Item.Saw)
+        {
+            EditorGUILayout.PropertyField(sawLogProperty);
+        }
+
+        serializedObject.ApplyModifiedProperties();
+    }
+}
