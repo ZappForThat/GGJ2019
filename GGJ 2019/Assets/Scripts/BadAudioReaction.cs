f25c0dc92ecac68876913dbd4b43b9045c64d357
Avery Pratt <apratt91@gmail.com>
2019-01-27 14:45:43 -0800

    more sound integration

---
+++ GGJ 2019/Assets/Scripts/BadAudioReaction.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BadAudioReaction : MonoBehaviour
+{
+    private void OnEnable()
+    {
+        AudioManager.Instance?.VictoryMusicPlay();
+    }
+}
