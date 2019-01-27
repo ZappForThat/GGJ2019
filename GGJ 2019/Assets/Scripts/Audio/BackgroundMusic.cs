5f91a60618d0b7d8715ef048bc9fc519db494242
Pantzmcgee <pantzmcgee@gmail.com>
2019-01-26 22:31:32 -0800

    Wwise updates

--- GGJ 2019/Assets/Scripts/Audio/BackgroundMusic.cs
+++ GGJ 2019/Assets/Scripts/Audio/BackgroundMusic.cs
@@ -4,15 +4,68 @@ using UnityEngine;
 
 public class BackgroundMusic : MonoBehaviour
 {
+    bool Menu = false;
+    bool Shop = false;
+    bool Building = false;
+    bool Victory = false;
+    float timer = 0f;
+    float randBuildingIncrement = -100f;
+
     // Start is called before the first frame update
     void Start()
     {
-        AkSoundEngine.PostEvent("BuildingMusic", gameObject);
+        ShopMusicPlay();
+        //BuildingMusicPlay();
+        Building = true;
     }
 
     // Update is called once per frame
-    void Update()
+    /*void Update()
+    {
+        if (Building)
+        {
+            timer += Time.deltaTime;
+            Debug.Log(timer);
+            if (timer > 2)
+            {
+                timer = 0.0f;
+                randBuildingIncrement += .1f; 
+                AkSoundEngine.SetRTPCValue("BuildingMusicIncrease", randBuildingIncrement);
+                Debug.Log("Hi");
+            }
+        }
+    }*/
+    void MenuMusic()
+    {
+
+    }
+    void ShopMusic()
+    {
+
+    }
+    void BuildingMusic()
     {
         
     }
+    void VictoryMusic()
+    {
+
+    }
+    void MenuMusicPlay()
+    {
+        AkSoundEngine.PostEvent("MenuMusic", gameObject);
+    }
+    void ShopMusicPlay()
+    {
+        AkSoundEngine.PostEvent("ShopMusic", gameObject);
+    }
+    void BuildingMusicPlay()
+    {
+        AkSoundEngine.PostEvent("BuildingMusic", gameObject);
+    }
+    void VictoryMusicPlay()
+    {
+        AkSoundEngine.PostEvent("VictoryMusic", gameObject);
+    }
+
 }
