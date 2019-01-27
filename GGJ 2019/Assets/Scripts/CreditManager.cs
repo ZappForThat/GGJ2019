e4ee7f2bbc3fc8ae89b7ce799975efcd06c4abcc
Avery Pratt <apratt91@gmail.com>
2019-01-27 03:12:02 -0800

    added a pretty decent credits scene

---
+++ GGJ 2019/Assets/Scripts/CreditManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreditManager : MonoBehaviour
+{
+    public Credit[] Credits { get; private set; }
+    public float Speed = 20;
+    public float Spacing = 80;
+
+    private void Start()
+    {
+        Credits = GetComponentsInChildren<Credit>();
+        OrganizeCredits();
+    }
+
+    private void Update()
+    {
+        transform.Translate(new Vector3(0, Speed * Time.deltaTime));
+    }
+
+    private void OrganizeCredits()
+    {
+        float i = 0;
+        foreach (Credit credit in Credits)
+        {
+            credit.transform.Translate(new Vector3(0, i));
+            i -= Spacing;
+        }
+    }
+}
